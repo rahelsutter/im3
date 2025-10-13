bf7d668124ef60c0fc1aacdd847564d4368ead97
rahelsutter <rahel.sutter@stud.fhgr.ch>
2025-10-13 13:10:41 +0200

    Sachen in Java/PHP

---
+++ backend/api/getByDate.php
@@ -0,0 +1,31 @@
+<?php
+
+// -> Datenbank Zugangsdaten einbinden
+require_once '../config.php';
+
+// -> json aktivieren
+header('Content-Type: application/json');
+
+// -> Verbindung mit der Datenbank
+try {
+    // -> Login auf Datenbank
+    $pdo = new PDO($dsn, $username, $password, $options);
+
+    $date = $_GET['date'];
+
+    //-> sql statement schreiben
+    $sql = "SELECT * FROM im3_air_pollution WHERE DATE(timestamp) = :date";
+    $stmt = $pdo->prepare($sql);
+
+    // -> sql statement ausführen
+    $stmt->execute( ['today' => $date ] );
+
+    // -> daten in empfang nehmen
+    $results = $stmt->fetchAll();
+
+    //-> daten als json zurückgeben
+    echo json_encode($results);
+
+} catch (PDOException $e) {
+    die("Verbindung zur Datenbank konnte nicht hergestellt werden: " . $e->getMessage());
+    }
---
+++ script.js
@@ -0,0 +1,30 @@
+async function getAll() {
+    const url = 'https://im3.uv-index-usa.com/backend/api/getAll.php';
+try {
+    const response = await fetch(url); //-> await: code muss warten bis API zurück kommt?
+    const data = await response.json();
+    console.log(data); // gibt die Daten der API in der Konsole aus
+} catch (error) {
+    console.error(error)
+    }
+}
+
+getAll()
+
+async function getByDate(date) {
+        const url = `https://im3.uv-index-usa.com/backend/api/getByDate.php?date=${date}`;
+try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data); // gibt die Daten der API in der Konsole aus
+} catch (error) {
+    console.error(error)
+    }
+}
+
+
+const datepicker = document.querySelector('#datepicker');
+datepicker.addEventListener('change', function() {
+    const date = datepicker.value;
+    console.log(date);
+})
